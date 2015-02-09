9fe0965ac36c7803ee69cb6b05f08a64e24d82d5
gabrielmoura23 <gabrielandrade.moura@gmail.com>
2015-02-09 11:13:08 -0300

    Primeira versão.

---
+++ IAPromocoes.Application/ViewModels/ItemPedidoViewModel.cs
@@ -0,0 +1,53 @@
+﻿using IAPromocoes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IAPromocoes.Application.ViewModels
+{
+    public class ItemPedidoViewModel
+    {
+        public ItemPedidoViewModel()
+        {
+            
+        }
+
+        [Key]
+        public Guid IdItemPedido { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo [{0}]")]
+        [DisplayName("ID Pedido")]
+        public Guid IdPedido { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo [{0}]")]
+        [DisplayName("ID Produto")]
+        public Guid IdProduto { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo [{0}]")]
+        [RegularExpression(@"^(0|\d{0,16}(\.\d{0,2})?)$")]
+        [DisplayName("Valor Unitário")]
+        public decimal ValorUnitario { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo [{0}]")]
+        [Range(0, int.MaxValue, ErrorMessage = "Digite apenas números no campo [{0}]")]
+        //[RegularExpression("([0-9]+)")]
+        [DisplayName("Quantidade")]
+        public int QtdProduto { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Guid IdUsuarioCadastro { get; set; }
+
+        [ScaffoldColumn(false)]
+        public System.DateTime DtCadastro { get; set; }
+
+        [ScaffoldColumn(false)]
+        public System.Nullable<Guid> IdUsuarioAlteracao { get; set; }
+
+        [ScaffoldColumn(false)]
+        public System.Nullable<System.DateTime> DtAlteracao { get; set; }
+
+        public PedidoViewModel PedidoViewModel { get; set; }
+        public ProdutoViewModel ProdutoViewModel { get; set; }
+    }
+}
---
+++ IAPromocoes.UI.MVC/Controllers/PedidoController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IAPromocoes.Application.ViewModels;
+using IAPromocoes.Application.Interfaces;
+
+namespace IAPromocoes.UI.MVC.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoAppService _pedidoApp;
+        private readonly IProdutoAppService _produtoApp;
+
+        public PedidoController(
+            IPedidoAppService pedidoApp,
+            IProdutoAppService produtoApp)
+        {
+            _pedidoApp = pedidoApp;
+            _produtoApp = produtoApp;
+        }
+
+        // GET: Pedido
+        public ActionResult Index()
+        {
+            var pedidoViewModel = _pedidoApp.GetAll();
+            return View(pedidoViewModel);
+        }
+
+        // GET: Pedido/Details/5
+        public ActionResult Details(Guid id)
+        {
+            var pedidoViewModel = _pedidoApp.GetById(id);
+            //pedidoViewModel.Enderecos = _enderecoApp.ObterPorCliente(id);
+
+            return View(pedidoViewModel);
+        }
+
+        // GET: Pedido/Create
+        public ActionResult Create()
+        {
+            //ViewBag.EstadoId = new SelectList(_estadoApp.GetAll().OrderBy(e => e.Nome), "EstadoId", "Nome");
+            //ViewBag.CidadeId = new SelectList(_cidadeApp.GetAll().OrderBy(c => c.Nome), "CidadeId", "Nome");
+
+            return View();
+        }
+
+        // POST: Pedido/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PedidoViewModel pedidoViewModel)
+        {
+            //ViewBag.EstadoId = new SelectList(_estadoApp.GetAll().OrderBy(e => e.Nome), "EstadoId", "Nome", clienteEndereco.EstadoId);
+            //ViewBag.CidadeId = new SelectList(_cidadeApp.GetAll().OrderBy(c => c.Nome), "CidadeId", "Nome", clienteEndereco.CidadeId);
+
+            if (ModelState.IsValid)
+            {
+                var result = _pedidoApp.Add(pedidoViewModel);
+
+                if (!result.IsValid)
+                {
+                    foreach (var validationAppError in result.Erros)
+                    {
+                        ModelState.AddModelError(string.Empty, validationAppError.Message);
+                    }
+                    return View(pedidoViewModel);
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(pedidoViewModel);
+        }
+
+        // GET: Pedido/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            var pedidoViewModel = _pedidoApp.GetById(id);
+
+            return View(pedidoViewModel);
+        }
+
+        // POST: Pedido/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PedidoViewModel pedidoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _pedidoApp.Update(pedidoViewModel);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(pedidoViewModel);
+        }
+
+        // GET: Pedido/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            var pedidoViewModel = _pedidoApp.GetById(id);
+
+            return View(pedidoViewModel);
+        }
+
+        // POST: Pedido/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            var model = _pedidoApp.GetById(id);
+            _pedidoApp.Remove(model);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult AdicionarCarrinho(Guid idProduto, Guid idProdutoPreco)
+        {
+            // Ao invés de colocar uma lista de ítens de Design, vamos colocar
+            // Um objeto da entidade Pedido, que já possui List<ItemDesign>.
+            // List<ItemDesign> carrinho = Session["Carrinho"] != null ? (List<ItemDesign>)Session["Carrinho"] : new List<ItemDesign>();
+            var carrinho = Session["Carrinho"] != null ? (PedidoViewModel) Session["Carrinho"] : new PedidoViewModel();
+            var produtoViewModel = _produtoApp.GetById(idProduto);
+
+            if (produtoViewModel != null)
+            {
+                //pego o Preço escolhido do produto
+                var produtoPrecoViewModel = produtoViewModel.ProdutoPrecosViewModel.Where(t => t.IdProdutoPreco == idProdutoPreco).FirstOrDefault();
+                if (produtoPrecoViewModel != null)
+                {
+                    var itemPedido = new ItemPedidoViewModel();
+                    //itemPedido.IdItemPedido = Guid.NewGuid();
+                    itemPedido.IdPedido = carrinho.IdPedido;
+                    itemPedido.IdProduto = idProduto;
+                    itemPedido.QtdProduto = 1;
+                    itemPedido.ValorUnitario = produtoPrecoViewModel.ValorUnitario;
+
+                    if (carrinho.ItensPedido.FirstOrDefault(x => x.IdProduto == produtoViewModel.IdProduto) != null)
+                        carrinho.ItensPedido.FirstOrDefault(x => x.IdProduto == produtoViewModel.IdProduto).QtdProduto += 1;
+                    else
+                        carrinho.ItensPedido.Add(itemPedido);
+
+                
+                    // Aqui podemos fazer o cálculo do valor
+                    //carrinho.ValorTotal = carrinho.ItensDesign.Select(i => i.Design).Sum(d => d.Preco);
+                    carrinho.ValorTotal = carrinho.ItensPedido.SelectMany(i => i.ProdutoViewModel.ProdutoPrecosViewModel).Sum(i => i.ValorUnitario);
+
+                    Session["Carrinho"] = carrinho;
+                }
+            }
+
+            return RedirectToAction("Carrinho");
+        }
+
+        public ActionResult Carrinho()
+        {
+            var carrinho = Session["Carrinho"] != null ? (PedidoViewModel)Session["Carrinho"] : new PedidoViewModel();
+
+            return View(carrinho);
+        }
+
+
+        public ActionResult ExcluirItem(Guid id)
+        {
+            var carrinho = Session["Carrinho"] != null ? (PedidoViewModel) Session["Carrinho"] : new PedidoViewModel();
+            var itemExclusao = carrinho.ItensPedido.FirstOrDefault(i => i.IdItemPedido == id);
+            carrinho.ItensPedido.Remove(itemExclusao);
+
+            Session["Carrinho"] = carrinho;
+            return RedirectToAction("Carrinho");
+        }
+
+        public ActionResult SalvarCarrinho()
+        {
+            var carrinho = Session["Carrinho"] != null ? (PedidoViewModel)Session["Carrinho"] : new PedidoViewModel();
+
+            //contexto.Pedidos.Add(carrinho);
+            //contexto.SaveChanges();
+
+            return RedirectToAction("Carrinho");
+        }
+    }
+}
