f0ca2315661ca684163f7361ff5b66adce7de367
gabrielmoura23 <gabrielandrade.moura@gmail.com>
2015-02-26 11:45:17 -0300

    startup

---
+++ IAPromocoes.UI.MVC/Startup.cs
@@ -0,0 +1,17 @@
+﻿using IAPromocoes.UI.MVC;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup("Startup", typeof(Startup))]
+namespace IAPromocoes.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
